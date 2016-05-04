5b2a7ad6375fe57f5242985cee82fb0148d2e430
zakpes <exelziorx@gmail.com>
2016-05-04 02:30:42 +0200

    Implemented images on hover

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function() {
     $panel.click(function() {
 
         var $this = $(this);
-        
+
         if ($this.children("span").hasClass("rotate-caret")) {
             $(".panel-heading span").removeClass("rotate-caret");
         } else {
@@ -13,6 +13,22 @@ $(function() {
             $this.children("span").addClass("rotate-caret");
         }
 
+    })
+
+    $(".panel-body img").hide();
+
+    $(".panel-body ul li").mouseenter(function() {
+
+        var $this = $(this);
+
+        $this.next("img").fadeIn("fast");
+    })
+
+    $(".panel-body ul li").mouseleave(function() {
+
+        var $this = $(this);
+
+        $this.next("img").fadeOut("fast");
     })
 
 })
